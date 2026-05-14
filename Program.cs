2411ee6d5a1a6405cebfdc3953b730acc1af6b44
Khin Thandar Tun <khinthandartun@kkaoyama.co.jp>
2026-05-14 09:49:27 +0900

    Update MCP

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using McpServer1.Services;
 using McpServer1.Tools;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,18 +9,18 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
 
 // Add the MCP services: the transport to use (stdio) and the tools to register.
-//builder.Services
-//    .AddMcpServer()
-//    .WithStdioServerTransport()
-//    .WithTools<RandomNumberTools>();
-
 builder.Services
     .AddMcpServer()
-    .WithStdioServerTransport()   // input/output (console) - use the existing API name
-    .WithTools<FixCodeTool>(); // specify the tool type parameter to satisfy the generic method
+    .WithStdioServerTransport()
+    .WithTools<WeatherTool>();
+
+//builder.Services
+//    .AddMcpServer()
+//    .WithStdioServerTransport()   // input/output (console) - use the existing API name
+//    .WithTools<FixCodeTool>(); // specify the tool type parameter to satisfy the generic method
 
 var app = builder.Build();
 
-Console.WriteLine("MCP Server Started..."); // debug
+Console.Error.WriteLine("MCP Server Started..."); // debug
 
 app.Run();
---
+++ Tools/WeatherTool.cs
@@ -0,0 +1,21 @@
+﻿using ModelContextProtocol.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McpServer1.Tools
+{
+    [McpServerToolType]
+    public class WeatherTool
+    {
+        [McpServerTool]
+        [Description("Gets Weather information for a specified location.")]
+        public string GetWeather(string city)
+        {
+            return $"Weather in {city}: Sunny, 25°C";
+        }
+    }
+}
